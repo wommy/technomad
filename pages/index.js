95c7ec9bfbf012204ab610516b2ee3de6b29a48d
wommy <tom@technomad.media>
2019-09-03 14:37:37 -0400

    git init next
    trying to learn next without first knowing gatsby
    gatsby is going to teach me react and graphql
    	i dont need to use graphql, but then i can upgrade to it
    then express server learning, is gonna get used by next

---
+++ pages/index.js
@@ -0,0 +1,177 @@
+function Home() {
+  return (
+    <main>
+      <article id="">
+      	<img className="wommyImg" src="https://d33wubrfki0l68.cloudfront.net/d01513d8a29841c9482b13c1ffb0fe781afcecbf/789b7/img/wommym.jpg" />
+        
+        <h2>welcome to my portfolio</h2>
+        
+        <p>feel free to check out</p>
+        
+        <a className='block indent1' href='/media'>my media</a>
+        
+        <a className="block indent1" href="experience/clients.html">my clients</a>
+        
+        <p className="indent1">my experiments</p>
+        
+        <a className="block indent2" href="experiments/feed.html">feed</a>
+        
+        <a className="block indent2" href="experiments/grid.html">grid</a>
+      </article>
+
+      <article id="inspiration">
+        <h2>
+        	<a href="#inspiration">inspiration / philosophy</a>
+        </h2>
+        <div>
+          <p>words</p>
+          <ul className="list-none">
+            <li>simple</li>
+            <li>concise</li>
+            <li>clean</li>
+            <li>eloquent</li>
+            <li>minimal</li>
+            <li>transparency</li>
+          </ul>
+        </div>
+        <div>
+          <p>websites</p>
+          
+          <div className="indent1">
+          	<a href="//facsrv.cdm.depaul.edu/~cmiller/">//facsrv.cdm.depaul.edu/~cmiller/</a>
+            <p className="subtext">- a teacher of mine, one of the first websites i encountered that put <a href="//ims21.net/web_form_function.htm">function over form</a></p>
+          </div>
+          
+          <div className="indent1">
+          	<a href="//itemsandthings.com/">//itemsandthings.com/</a>
+            <p className="subtext">-  one of my favorite music labels. their brand's dedication to minimalism is conveyed thoroughly, from their music to their web design.</p>
+          </div>
+          
+          <div className="indent1">
+          	<a href="//www.wanna-rework.de/">//www.wanna-rework.de/</a>
+            <p className="subtext">- one of the musicians websites on the above label</p>
+          </div>
+        </div>
+        
+        <div>
+          <p>quotes</p>
+          
+          <a href="//www.reddit.com/r/terencemckenna/comments/32rp8t/a_quote_that_really_grabbed_me_from_terences/">
+          	<img src="//i.imgur.com/0MAncfi.png" width="90%" />
+          </a>
+        </div>
+      </article>
+
+      <article id="hobbies">
+        <h1>
+        	<a href="#hobbies">hobbies :: aspects that define me</a>
+        </h1>
+        
+        <hr />
+        
+        <div id="biking">
+          <h2 id="biking">biking</h2>
+          <p>this will be my 6th year biking in chicago</p>
+          <p>check out my biking video <a href="media.html#create a fresh path">here</a></p>
+        </div>
+        
+        <div id="kungfu">
+          <h2 id="kungfu">kungfu</h2>
+          <p>ive been practicing kungfu for over a year now</p>
+          <p>
+          	<a href='/media#wingchun'>i have a couple videos on my media page</a>
+          </p>
+          <p>
+          	ive also been working on their website<br />
+          	you can see the entry on my <a href="experience/clients.html#wingChun">client page here</a>
+          </p>
+        </div>
+        
+        <div id="psych">
+          <h2 id="psych">psych</h2>
+          
+          <p>my psych career is long and winding</p>
+          
+          <p>my first introduction to psychology was through <a href="//en.wikipedia.org/wiki/Myers%E2%80%93Briggs_Type_Indicator">myers briggs</a></p>
+          
+          <p>hours of trawling google led me to</p>
+          
+          <h3>socionics</h3>
+          
+          <p>socionics was everything i was looking for</p>
+          
+          <p>
+          	socionics' <a href="//www.wikisocion.org/en/index.php?title=Rationality_and_irrationality#Typical_characteristics">rationality / irrationality </a>replaces mbti's judgment / perception<br />
+          	mbti's judgment / perception still exists within socionics, but it is instead a second level dichotomy, <a href="//www.wikisocion.org/en/index.php?title=Static_and_dynamic#Typical_characteristics">static / dynamic</a>
+          </p>
+          
+          <p>socionics goes into much greater detail than myers briggs:</p>
+        	
+        	<ul>
+          	<li><a href="//www.wikisocion.org/en/index.php?title=Intertype_relations">intertype relations </a>- descriptions of interactions between types</li>
+          	<li><a href="//www.wikisocion.org/en/index.php?title=Small_groups">small groups </a>- common ground between types</li>
+          	<li><a href="//www.wikisocion.org/en/index.php?title=Subtypes">subtypes </a>- no two types are the same</li>
+          	<li><a href="//www.wikisocion.org/en/index.php?title=Model_A">model a </a>-</li>
+          	<li><a href="//www.wikisocion.org/en/index.php?title=Reinin_dichotomies">second level  dichotomies</a></li>
+          	<li><a href="//www.wikisocion.org/en/index.php?title=Information_elements">information  elements</a></li>
+          	<li><a href="//www.wikisocion.org/en/index.php?title=Functions">functions </a>which allows for <a href="//www.wikisocion.org/en/index.php?title=Function_dichotomies">functional dichotomies </a></li>
+            <li>male female differentiations</li>
+        	</ul>
+          
+          <p>
+          	<a href="//www.wikisocion.org/en/index.php?title=Information_Metabolism">information metabolism </a>- can be simplied as follows: <br />
+          	every action you do, is based on information. 
+          </p>
+          
+          <h3>horoscopes</h3>
+          
+          <div>
+          	<a href="//www.thesecretlanguage.com/today/">the secret language</a><br />
+          	my number one to-go-to
+            <div className="indent1">
+              <p>
+              	here you can check <a href="//www.thesecretlanguage.com/check/birthdate/">your or anothers birthday </a><br />
+              	( check you and a parent )<br />
+              	and <a href="//www.thesecretlanguage.com/check/relationship/">your relationships with other people</a><br />
+              	( check an old or current flame )
+              </p>
+            </div>
+          </div>
+          
+          <div>
+          	<a href="//astro.cafeastrology.com/">cafe astrology</a>
+            <div className="indent1">
+              <p>
+              	i personally like their <a href="//astro.cafeastrology.com/cgi-bin/astro/natal">natal chart</a><br />
+              	and their <a href="//astro.cafeastrology.com/cgi-bin/astro/comp2f">relationship chart</a>
+              </p>
+            </div>
+          </div>
+          
+          <h3>tarot year</h3>
+          
+          <a href="//www.tarotschool.com/Calculator.html">heres a easy calculator i just found</a>
+          
+          <div className="indent1">
+            <p>
+            	for example:<br />
+            	i want to check for my current year<br />
+            	since my birthday has not happened yet, i check for last year
+            </p>
+          </div>
+          
+          <p>
+          	<a className="block" href="//accessnewage.com/articles/Tarot/lottar2.htm">long description</a>
+          	<a className="block" href="//www.cosmictarot.co.uk/discover-your-personal-year-card/">short description</a>
+          </p>
+          
+          <a className="block" href="//thetarotroom.com/tarot-growth-cards/">this site has more descriptions</a>
+          
+          <p className="subtext">but you have to know your number, use calc above</p>
+        </div>
+      </article>  
+    </main>
+  );
+}
+
+export default Home;
